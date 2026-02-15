ac66120f32dd670dadfad8038bb837405b53937a
Matteo Tortelli <tortellimatteo@gmail.com>
2026-02-15 11:19:03 +0100

    feat: aggiunti nuovi file icone e manifest per migliorare l'esperienza utente

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "evo · boards",
+  title: "Trello Boards",
   description: "Visualizza le card di tutte le board di Trello",
 };
 
